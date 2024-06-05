4e0c85ccda78020e4ae9c0a6eccc308584c92547
Ангелинка <145559226+Vupsen73@users.noreply.github.com>
2024-06-05 22:53:51 +0300

    Add files via upload

---
+++ prj.codeforces/0546a.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int k, n, w = 0;
+    cin >> k >> n >> w;
+    int a = k * w * (w + 1) / 2;
+    if (a > n) {
+        cout << a - n;
+    }
+    else cout << 0;
+}

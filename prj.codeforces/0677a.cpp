c8339e0d7e56fd9a27908a6c5a2b11742911b060
Ангелинка <145559226+Vupsen73@users.noreply.github.com>
2024-06-06 15:45:46 +0300

    Add files via upload

---
+++ prj.codeforces/0677a.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n, h, a, ans = 0;
+    cin >> n >> h;
+    while (n--)
+    {
+        cin >> a;
+        if (a > h)
+            ans += 2;
+        else
+            ans++;
+    }
+    cout << ans << endl;
+    return 0;
+}

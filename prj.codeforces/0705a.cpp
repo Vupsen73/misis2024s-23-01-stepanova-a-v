3853a0ac5ad7e771031923ba7f3734918c22f803
Ангелинка <145559226+Vupsen73@users.noreply.github.com>
2024-06-07 17:05:01 +0300

    Update 0705a.cpp

--- prj.codeforces/0705a.cpp
+++ prj.codeforces/0705a.cpp
@@ -1,20 +1,25 @@
 #include<iostream>
-using namespace std;
+#include<string>
+
 int main()
 {
 	int n;
-	cin >> n;
-	string s;
+	std::cin >> n;
+	std::string s;
 	for (int i = 1; i <= n; i++)
 	{
 		if (i == 1)
+		{
 			s = "I hate ";
+		}
 		else if (i % 2 == 0)
 		{
 			s = s + "that I love ";
 		}
 		else
+		{
 			s = s + "that I hate ";
+		}
 	}
-	cout << s << "it";
+	std::cout << s << "it";
 }

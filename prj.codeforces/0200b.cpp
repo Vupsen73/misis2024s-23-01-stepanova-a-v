f41022aac272c1e92d173146f826f67a71cb36a2
Ангелинка <145559226+Vupsen73@users.noreply.github.com>
2024-06-07 16:44:34 +0300

    Update 0200b.cpp

--- prj.codeforces/0200b.cpp
+++ prj.codeforces/0200b.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
-using namespace std;
+
 int main()
 {
-	double a, s = 0, n;
-	cin >> n;
+	double n;
+	double a;
+	double s = 0;
+	std::cin >> n;
 	for (int i = 0; i < n; i++) {
-		cin >> a;
+		std::cin >> a;
 		s += (a / 100);
 	}
-	cout << s / n * 100 << endl;
+	std::cout << s / n * 100 << std::endl;
+	
 	return 0;
 }

1a01f443ebf7e41eeef526cd7e6229b85d72fb36
Ангелинка <145559226+Vupsen73@users.noreply.github.com>
2024-06-07 16:56:17 +0300

    Update 0339a.cpp

--- prj.codeforces/0339a.cpp
+++ prj.codeforces/0339a.cpp
@@ -1,14 +1,18 @@
-#include <iostream>
-#include <algorithm>
-using namespace std;
-int main() {
-    string s, p = "";
-    cin >> s;
+#include<iostream>
+#include<algorithm>
+#include<string>
+
+int main() 
+{
+    std::string s = "";
+    std::string p = "";
+    std::cin >> s;
     sort(s.begin(), s.end());
-    for (int i = (s.size() / 2); i < s.size(); i++) {
+    for (int i = (s.size() / 2); i < s.size(); i++) 
+    {
         p += s[i];
         p += '+';
     }
     p.pop_back();
-    cout << p;
+    std::cout << p;
 }

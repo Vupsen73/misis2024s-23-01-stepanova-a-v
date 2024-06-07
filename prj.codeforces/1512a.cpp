fff992609ff8635dc175f4ec026fdf1780438817
Ангелинка <145559226+Vupsen73@users.noreply.github.com>
2024-06-07 18:16:31 +0300

    Add files via upload

---
+++ prj.codeforces/1512a.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+
+int main() 
+{
+    int a;
+    std::cin >> a;
+    while (a != 0) {
+        int b;
+        std::cin >> b;
+        int arr[b];
+        for (int i = 0; i < b; i++) {
+            std::cin >> arr[i];
+        }
+        for (int i = 0; i < b; i++) {
+            int c = std::count(arr, arr + b, arr[i]);
+            if (c == 1) {
+                std::cout << i + 1 << std::endl;
+            }
+        }
+        a--;
+    }
+    return 0;
+}

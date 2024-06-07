28ab1affcafdc6be8d480b02444e9bc640861744
Ангелинка <145559226+Vupsen73@users.noreply.github.com>
2024-06-07 18:33:33 +0300

    Add files via upload

---
+++ prj.codeforces/1560a.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+int main()
+{
+    int t, n, i, cnt;
+    std::cin >> t;
+    while (t--)
+    {
+        cnt = 1;
+        i = 1;
+        std::cin >> n;
+        while (cnt <= n)
+        {
+            if (i % 10 != 3 && i % 3 != 0)
+            {
+                cnt++;
+            }
+            i++;
+        }
+        std::cout << --i << std::endl;
+    }
+}

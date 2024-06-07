6def43cb282a1e95001cddc49553296528e5f3e6
Ангелинка <145559226+Vupsen73@users.noreply.github.com>
2024-06-07 16:49:31 +0300

    Update 0236a.cpp

--- prj.codeforces/0236a.cpp
+++ prj.codeforces/0236a.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <set>
 #include <string>
-using namespace std;
+
 int main()
 {
-    string a;
-    cin >> a;
-    set<char>b(a.begin(), a.end());
+    std::string a;
+    std::cin >> a;
+    std::set<char>b(a.begin(), a.end());
     if (b.size() % 2 == 0)
-        cout << "CHAT WITH HER!";
+    {
+        std::cout << "CHAT WITH HER!";
+    }
     else
-        cout << "IGNORE HIM!";
+    {
+        std::cout << "IGNORE HIM!";
+    }
 }

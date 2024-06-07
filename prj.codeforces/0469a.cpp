4e56d56a82cfd16398256cbc5c31350a5febb50d
Ангелинка <145559226+Vupsen73@users.noreply.github.com>
2024-06-07 16:59:32 +0300

    Update 0469a.cpp

--- prj.codeforces/0469a.cpp
+++ prj.codeforces/0469a.cpp
@@ -1,26 +1,31 @@
 #include<iostream>
 #include<set>
-using namespace std;
 
 int main()
 {
-    int n, w, x, y;
-    cin >> n;
-    set<int> s;
-    cin >> x;
+    int n;
+    int w;
+    int x;
+    int y;
+    std::cin >> n;
+    std::set<int> s;
+    std::cin >> x;
     while (x--)
     {
-        cin >> w;
+        std::cin >> w;
         s.insert(w);
     }
-    cin >> y;
+    std::cin >> y;
     while (y--) {
-        cin >> w;
+        std::cin >> w;
         s.insert(w);
     }
     if (s.size() == n)
     {
-        cout << "I become the guy.";
+        std::cout << "I become the guy.";
     }
-    else cout << "Oh, my keyboard!";
+    else
+    {
+        std::cout << "Oh, my keyboard!";
+    }
 }

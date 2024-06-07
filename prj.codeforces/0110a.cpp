55276d9e8021ee4fc94f074eaf649aabed7d7f2c
Ангелинка <145559226+Vupsen73@users.noreply.github.com>
2024-06-07 15:52:44 +0300

    Update 0110a.cpp

--- prj.codeforces/0110a.cpp
+++ prj.codeforces/0110a.cpp
@@ -1,15 +1,24 @@
 #include<iostream>
 #include<string>
-using namespace std;
+
 
 int main()
 {
-	string s;
-	cin >> s;
+	std::string s;
+	std::cin >> s;
 	int ans = 0;
 	for (int i = 0; i < s.length(); i++)
-		if (s[i] == '4' or s[i] == '7') ans++;
-	if (ans == 4 or ans == 7) cout << "YES" << endl;
-	else cout << "NO" << endl;
+		if (s[i] == '4' or s[i] == '7')
+		{
+			ans++;
+		}
+	if (ans == 4 or ans == 7)
+	{
+		std::cout << "YES" << std::endl;
+	}
+	else
+	{
+		std::cout << "NO" << std::endl;
+	}
 	return 0;
 }

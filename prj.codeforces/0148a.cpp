8b98bdb63e88f501cfc07fd4c6f175ccaab65854
Ангелинка <145559226+Vupsen73@users.noreply.github.com>
2024-06-07 15:58:21 +0300

    Update 0148a.cpp

--- prj.codeforces/0148a.cpp
+++ prj.codeforces/0148a.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
-using namespace std;
+
 int main()
 {
-    int k, l, m, n, d;
-    cin >> k >> l >> m >> n >> d;
+    int k;
+    int l;
+    int m;
+    int n;
+    int d;
+    std::cin >> k >> l >> m >> n >> d;
     int c = 0;
     for (int i = 1; i <= d; i++)
     {
         if (i % k == 0 or i % l == 0 or i % m == 0 or i % n == 0)
+        {
             c++;
+        }
     }
-    cout << c;
+    std::cout << c;
 }
